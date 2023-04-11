6ece2baef7d02953f9861ee6b128680f492427e2
OlhaNekrylova <nekrylovaurist@gmail.com>
2023-04-11 18:44:35 +0300

    added firebaseConfig

--- my-app-06/App.js
+++ my-app-06/App.js
@@ -1,14 +1,18 @@
 import React from "react";
-
+import { Provider } from "react-redux";
 import { NavigationContainer } from '@react-navigation/native';
 
 import Navigation from "./router";
+import { store } from "./redux/store";
 
 export default function App() {
   
   return (
-    <NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
           <Navigation/>         
       </NavigationContainer>
+    </Provider>
+    
   );
 }
